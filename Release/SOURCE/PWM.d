3f2640edf73c3745598081c47f8b1b0698cf163d
Linky141 <tomasz_bielas@yahoo.com>
2020-05-12 00:41:34 +0200

    first

---
+++ Release/SOURCE/PWM.d
@@ -0,0 +1,6 @@
+SOURCE/PWM.o: ../SOURCE/PWM.c ../SOURCE/../HEADERS/PWM.h \
+ ../SOURCE/../HEADERS/../Libraries/Headers/stdutils.h
+
+../SOURCE/../HEADERS/PWM.h:
+
+../SOURCE/../HEADERS/../Libraries/Headers/stdutils.h:
---
+++ Release/SOURCE/main.d
@@ -0,0 +1,23 @@
+SOURCE/main.o: ../SOURCE/main.c ../SOURCE/../Libraries/Headers/uart.h \
+ ../SOURCE/../Libraries/Headers/stdutils.h \
+ ../SOURCE/../Libraries/Headers/delay.h \
+ ../SOURCE/../Libraries/Headers/lcd.h \
+ ../SOURCE/../Libraries/Headers/gpio.h ../SOURCE/../HEADERS/pwm.h \
+ ../SOURCE/../HEADERS/../Libraries/Headers/stdutils.h \
+ ../SOURCE/../Libraries/Headers/eeprom.h
+
+../SOURCE/../Libraries/Headers/uart.h:
+
+../SOURCE/../Libraries/Headers/stdutils.h:
+
+../SOURCE/../Libraries/Headers/delay.h:
+
+../SOURCE/../Libraries/Headers/lcd.h:
+
+../SOURCE/../Libraries/Headers/gpio.h:
+
+../SOURCE/../HEADERS/pwm.h:
+
+../SOURCE/../HEADERS/../Libraries/Headers/stdutils.h:
+
+../SOURCE/../Libraries/Headers/eeprom.h:
